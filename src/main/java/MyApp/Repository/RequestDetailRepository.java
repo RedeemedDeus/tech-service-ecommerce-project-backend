af53b0b394a3800a3c80da14065c11eb7131db66
Alexa Lewis <alexalewis2013@afscholars.org>
2023-03-21 15:57:04 -0400

    requestDetail repo and model files

---
+++ src/main/java/MyApp/Repository/RequestDetailRepository.java
@@ -0,0 +1,6 @@
+package MyApp.Repository;
+
+import MyApp.Model.RequestDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface RequestDetailRepository extends JpaRepository<RequestDetail, Long>{
+}
